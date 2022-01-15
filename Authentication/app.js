00fb8bbe5bd5140cd68c4669e142fffe74198971
Hamza Ilyas <hamza24061@gmail.com>
2022-01-16 03:01:45 +0500

    Firebase Authentication --Version 09

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,4 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBpJtwtujHE__np4Gjn3myaTEGOm4-U214",
@@ -6,14 +11,43 @@ const firebaseConfig = {
   projectId: "auth-test-a7bec",
   storageBucket: "auth-test-a7bec.appspot.com",
   messagingSenderId: "932496717264",
-  appId: "1:932496717264:web:501794417fd5b770a735d5"
+  appId: "1:932496717264:web:501794417fd5b770a735d5",
 };
 
 const app = initializeApp(firebaseConfig);
 
+const auth = getAuth();
 
-let loginEmail = document.getElementById('loginEmail');
-let loginPassword = document.getElementById('loginPassword');
-let registerEmail = document.getElementById('registerEmail');
-let registerPassword = document.getElementById('registerPassword');
+let registerBtn = document.getElementById("registerBtn");
 
+registerBtn.addEventListener("click", function () {
+  let registerEmail = document.getElementById("registerEmail").value;
+  let registerPassword = document.getElementById("registerPassword").value;
+  createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("user ==>", user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("error ==>", errorMessage);
+    });
+});
+
+let loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", function () {
+  let loginEmail = document.getElementById("loginEmail").value;
+  let loginPassword = document.getElementById("loginPassword").value;
+  signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("user ==>", user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("error ==>", errorMessage);
+    });
+});
